787046568892b5c32ce294c5aada923387af4f86
jonas-amilton <jonas96@edu.unisinos.br>
2023-11-13 03:27:14 -0300

    [new] aulas de strings

---
+++ 10_strings/1_interpolacao/index.php
@@ -0,0 +1,8 @@
+<?php
+
+$idade = 27;
+
+
+echo "Eu tenho $idade anos. <br>";
+
+echo "Eu tenho {$idade} anos.<br>";
---
+++ 10_strings/2_valores_de_escape/index.php
@@ -0,0 +1,15 @@
+<?php
+
+header("Content-type: text/plain");
+
+// pular linha
+echo "Isso aqui vai pular linha \nSegunda Linha \n";
+
+// Tab
+echo "Testando o Tab \t aqui\n";
+
+// Barra invertida
+echo "Barra invertida \\ \n";
+
+//Dólar
+echo "Imprimindo o Dolar \$teste";
