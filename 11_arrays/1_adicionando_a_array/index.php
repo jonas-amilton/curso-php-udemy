8d8a0089aa5581c43dde64470d986218c041d0e8
jonas-amilton <jonas96@edu.unisinos.br>
2023-11-14 05:57:17 -0300

    [new] aulas de array

---
+++ 11_arrays/1_adicionando_a_array/index.php
@@ -0,0 +1,37 @@
+<?php
+
+$arr = [];
+
+print_r($arr);
+echo '<br>';
+
+// adicionando valores
+
+$arr[0] = 10;
+
+print_r($arr);
+echo '<br>';
+
+$arr[1] = 15;
+
+print_r($arr);
+echo '<br>';
+
+// modificando valores
+
+$arr[1] += 55;
+
+print_r($arr);
+echo '<br>';
+
+// array associativo
+
+$arrAssoc = [];
+
+print_r($arrAssoc);
+echo '<br>';
+
+$arrAssoc['Carro'] = 'Ferrari';
+
+print_r($arrAssoc);
+echo '<br>';
---
+++ 11_arrays/3_range/index.php
@@ -0,0 +1,14 @@
+<?php
+
+$arr = range(1, 10);
+
+print_r($arr);
+
+echo '<br>';
+
+// step definido em 100, logo valores serão [0, 100, 200, 300... 1000]
+$arr2 = range(0, 1000, 100);
+
+print_r($arr2);
+
+echo '<br>';
