2aa1833593ae04fd8d2a9f4ff4b6bceac73ea843
jonas-amilton <jonas96@edu.unisinos.br>
2023-11-15 02:25:19 -0300

    [fix] solução exercicio 47

---
+++ 11_arrays/9_exercicio_47/index.php
@@ -0,0 +1,7 @@
+<?php
+
+$carro = ['jaguar', 3.0, 'azul', 18, 'Teto solar', 'automático'];
+
+list($nome, $motor, $cor, $modelo, $teto, $cambio) = $carro;
+
+echo "O $nome possui motor $motor.0 e é $cor, o modelo é $modelo e possui $teto e cambio $cambio";
