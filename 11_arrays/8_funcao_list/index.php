839804a07053de870c06c5ded093215f123dceb2
jonas-amilton <jonas96@edu.unisinos.br>
2023-11-15 02:20:03 -0300

    [new] aula função list

---
+++ 11_arrays/8_funcao_list/index.php
@@ -0,0 +1,11 @@
+<?php
+
+$pessoa = ['Matheus', 29, 'Programador', 'verde'];
+
+
+list($nome, $idade, $profissao, $corDosOlhos) = $pessoa;
+
+echo $nome . '<br>';
+echo $idade . '<br>';
+echo $profissao . '<br>';
+echo $corDosOlhos . '<br>';
